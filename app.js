a9335ba9a736677a187bee35bca3094bd6780f2f
Jesus Farro <jesussfa20@gmail.com>
2025-02-25 01:55:20 -0500

    update app.js

--- app.js
+++ app.js
@@ -1,1 +1,22 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
+let amigos = [];
+
+function agregarAmigo (){
+    let candidatoAmigo = document.getElementById('amigo');
+    if (candidatoAmigo = ''){
+        alert('Por favor, inserte un nombre.');
+    } else {
+        amigos.push(candidatoAmigo);
+    }
+    campoVacio();
+}
+
+function campoVacio (){
+    document.getElementById('amigo').value = '';
+}
+
+function condicionesIniciales (){
+    campoVacio();
+    
+}
